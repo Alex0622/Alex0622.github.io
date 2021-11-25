be6fc6e5e720a35cd66617e5155fbdb0efa74b6e
Alex0622 <aleximora2206@gmail.com>
2021-11-25 11:19:09 -0600

    Update everything

--- Posts/script.js
+++ Posts/script.js
@@ -1,10 +1,2 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-});
 
-document.querySelector(".search-btn").addEventListener("click", () => {
-    document.querySelector(".msg-menu").classList.toggle("show");
-});
-
-ScrollReveal().reveal('.warning-banner');
-ScrollReveal().reveal('.posts-cards', { delay: 500 });
+ScrollReveal().reveal('.post-cards');
--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-});
 
-document.querySelector(".search-btn").addEventListener("click", () => {
-    document.querySelector(".msg-menu").classList.toggle("show");
-});
-
-ScrollReveal().reveal('.warning-banner');
-ScrollReveal().reveal('.news-cards', { delay: 500 });
-ScrollReveal().reveal('.card-banner-one', { delay: 500 });
-ScrollReveal().reveal('.card-banner-two', { delay: 500 });
+ScrollReveal().reveal('.intro');
+ScrollReveal().reveal('.art', { delay: 500 });
+ScrollReveal().reveal('.discordbots', { delay: 500 });
+ScrollReveal().reveal('.testprograms', { delay: 500 });
